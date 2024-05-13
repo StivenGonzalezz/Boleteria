c4973e9e5dbf8fb1946cb617fe83215932be3e3a
JulianAndresNaranjo <julianandresnaranjoalzate@gmail.com>
2024-05-13 17:35:29 -0500

    1,2,3,parte 4

---
+++ src/Logica/Main.java
@@ -0,0 +1,10 @@
+package Logica;
+
+import Opciones.Opciones;
+
+public class Main{
+    public static void main(String[] args) {
+        Opciones opciones= new Opciones();
+        opciones.ejecutar();
+    }
+}
